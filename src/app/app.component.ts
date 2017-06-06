b00afb9115321b69576ee7e853bd392096b74bc5
chgc <chgc_kevin@hotmail.com>
2017-06-06 23:45:24 +0800

    實作移除todo的功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,12 @@ export class AppComponent {
         {id: this.getId(), content: this.newTodo, isCompleted: false});
     this.newTodo = '';
   }
-
+  remove(todo) {
+    const idx = this.todoList.findIndex(x => x.id === todo.id);
+    if (idx > -1) {
+      this.todoList.splice(idx, 1);
+    }
+  }
   getId() {
     this.id++;
     return this.id;
